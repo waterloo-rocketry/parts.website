4c058bdc65a695aac2fd84c6726d8f35c84502e5
Kai Huang <137366044+CasualIntellectual@users.noreply.github.com>
2024-03-16 23:36:54 -0400

    Add files via upload

--- table.js
+++ table.js
@@ -5,21 +5,112 @@ function td(content) {
     return res;
 }
 
-function buildTable(data) {
+function buildTable(data, section) {
+    var w = window.innerWidth;
     let table = document.getElementById("parts-table");
     // Just replace the entire table with the given data
-    table.replaceChildren(table.children[0],
-        ...data.map(part => {
-            let tr = document.createElement("tr");
-            tr.replaceChildren(
-                td(formatValue(part)),
-                td(part.location),
-                td(part.description),
-                td(part.footprint),
-                td(formatTolerance(part)),
-                td(part.rating),
-            );
-            return tr;
-        })
-    );
+    if (w > 1000) {
+        table.replaceChildren(table.children[0],
+            ...data.map(part => {
+                let tr = document.createElement("tr");
+                tr.replaceChildren(
+                    td(formatValue(part)),
+                    td(part.location),
+                    td(part.description),
+                    td(part.footprint),
+                    td(formatTolerance(part)),
+                    td(part.ratingA),
+                    td(part.ratingV),
+                    td(part.ratingW),
+                );
+                return tr;
+            })
+        );
+        document.getElementById("section1").style.display = "none";
+        document.getElementById("section2").style.display = "none";
+        document.getElementById("section3").style.display = "none";
+        document.getElementById("section1a").style.display = "table-cell";
+        document.getElementById("section1b").style.display = "table-cell";
+        document.getElementById("section2a").style.display = "table-cell";
+        document.getElementById("section2b").style.display = "table-cell";
+        document.getElementById("section3a").style.display = "table-cell";
+        document.getElementById("section3b").style.display = "table-cell";
+        document.getElementById("section3c").style.display = "table-cell";
+    } else if (section == 1) {
+        table.replaceChildren(table.children[0],
+            ...data.map(part => {
+                let tr = document.createElement("tr");
+                tr.replaceChildren(
+                    td(formatValue(part)),
+                    td(part.location),
+                    td(part.description),
+                );
+                return tr;
+            })
+        );
+        document.getElementById("section1").style.display = "inline-block";
+        document.getElementById("section2").style.display = "inline-block";
+        document.getElementById("section3").style.display = "inline-block";
+        document.getElementById("section1").src = "image/selected.png";
+        document.getElementById("section2").src = "image/unselected.png";
+        document.getElementById("section3").src = "image/unselected.png";
+        document.getElementById("section1a").style.display = "table-cell";
+        document.getElementById("section1b").style.display = "table-cell";
+        document.getElementById("section2a").style.display = "none";
+        document.getElementById("section2b").style.display = "none";
+        document.getElementById("section3a").style.display = "none";
+        document.getElementById("section3b").style.display = "none";
+        document.getElementById("section3c").style.display = "none";
+    } else if (section == 2) {
+        table.replaceChildren(table.children[0],
+            ...data.map(part => {
+                let tr = document.createElement("tr");
+                tr.replaceChildren(
+                    td(formatValue(part)),
+                    td(part.footprint),
+                    td(formatTolerance(part)),
+                );
+                return tr;
+            })
+        );
+        document.getElementById("section1").style.display = "inline-block";
+        document.getElementById("section2").style.display = "inline-block";
+        document.getElementById("section3").style.display = "inline-block";
+        document.getElementById("section1").src = "image/unselected.png";
+        document.getElementById("section2").src = "image/selected.png";
+        document.getElementById("section3").src = "image/unselected.png";
+        document.getElementById("section1a").style.display = "none";
+        document.getElementById("section1b").style.display = "none";
+        document.getElementById("section2a").style.display = "table-cell";
+        document.getElementById("section2b").style.display = "table-cell";
+        document.getElementById("section3a").style.display = "none";
+        document.getElementById("section3b").style.display = "none";
+        document.getElementById("section3c").style.display = "none";
+    } else if (section == 3) {
+        table.replaceChildren(table.children[0],
+            ...data.map(part => {
+                let tr = document.createElement("tr");
+                tr.replaceChildren(
+                    td(formatValue(part)),
+                    td(part.ratingA),
+                    td(part.ratingV),
+                    td(part.ratingW),
+                );
+                return tr;
+            })
+        );
+        document.getElementById("section1").style.display = "inline-block";
+        document.getElementById("section2").style.display = "inline-block";
+        document.getElementById("section3").style.display = "inline-block";
+        document.getElementById("section1").src = "image/unselected.png";
+        document.getElementById("section2").src = "image/unselected.png";
+        document.getElementById("section3").src = "image/selected.png";
+        document.getElementById("section1a").style.display = "none";
+        document.getElementById("section1b").style.display = "none";
+        document.getElementById("section2a").style.display = "none";
+        document.getElementById("section2b").style.display = "none";
+        document.getElementById("section3a").style.display = "table-cell";
+        document.getElementById("section3b").style.display = "table-cell";
+        document.getElementById("section3c").style.display = "table-cell";
+    }
 }
